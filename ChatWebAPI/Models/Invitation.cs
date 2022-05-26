17f523cd3442211ef19977e12311a7469f52d690
omri rata <omri.rata@live.biu.ac.il>
2022-05-26 19:18:48 +0300

    added Invitation model

---
+++ ChatWebAPI/Models/Invitation.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatWebAPI.Models
+{
+    public class Invitation
+    {
+        public string? From { get; set; }
+        public string? To { get; set; }
+        public string? Server { get; set; }
+
+    }
+}
