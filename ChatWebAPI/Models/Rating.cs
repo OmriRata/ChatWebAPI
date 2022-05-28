79584a12ad921587f193430b5ec5792dc474105d
maorb138 <48924571+maorb138@users.noreply.github.com>
2022-05-28 17:32:38 +0300

    Add files via upload

---
+++ ChatWebAPI/Models/Rating.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatWebAPI.Models
+{
+    public class Rating
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public double? Rate { get; set; }
+        List<String>? RatingList { get; set; }
+    }
+}
