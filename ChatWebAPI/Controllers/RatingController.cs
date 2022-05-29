87a5a80efafa200ed970c669cbcee4cb96ec02a7
maorb138 <48924571+maorb138@users.noreply.github.com>
2022-05-29 17:14:32 +0300

    Upadte Rating controll

--- ChatWebAPI/Controllers/RatingController.cs
+++ ChatWebAPI/Controllers/RatingController.cs
@@ -1,14 +1,164 @@
-﻿using ChatWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ChatWebAPI.Data;
+using ChatWebAPI.Models;
+
 namespace ChatWebAPI.Controllers
 {
-    public class RatingController : Controller
+    public class RatingsController : Controller
     {
-        public IActionResult Index()
+        private readonly ChatWebAPIContext _context;
+
+        public RatingsController(ChatWebAPIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Ratings
+        public async Task<IActionResult> Index()
+        {
+              return _context.Rating != null ? 
+                          View(await _context.Rating.ToListAsync()) :
+                          Problem("Entity set 'ChatWebAPIContext.Rating'  is null.");
+        }
+
+        // GET: Ratings/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Rating == null)
+            {
+                return NotFound();
+            }
+
+            var rating = await _context.Rating
+                .FirstOrDefaultAsync(m => m.Name == id);
+            if (rating == null)
+            {
+                return NotFound();
+            }
+
+            return View(rating);
+        }
+
+        // GET: Ratings/Create
+        public IActionResult Create()
         {
             return View();
         }
-     
-     
+
+        // POST: Ratings/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Description,Rate")] Rating rating)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(rating);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rating);
+        }
+
+        // GET: Ratings/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Rating == null)
+            {
+                return NotFound();
+            }
+
+            var rating = await _context.Rating.FindAsync(id);
+            if (rating == null)
+            {
+                return NotFound();
+            }
+            return View(rating);
+        }
+
+        // POST: Ratings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Name,Description,Rate")] Rating rating)
+        {
+            if (id != rating.Name)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(rating);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RatingExists(rating.Name))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rating);
+        }
+
+        // GET: Ratings/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Rating == null)
+            {
+                return NotFound();
+            }
+
+            var rating = await _context.Rating
+                .FirstOrDefaultAsync(m => m.Name == id);
+            if (rating == null)
+            {
+                return NotFound();
+            }
+
+            return View(rating);
+        }
+
+        // POST: Ratings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Rating == null)
+            {
+                return Problem("Entity set 'ChatWebAPIContext.Rating'  is null.");
+            }
+            var rating = await _context.Rating.FindAsync(id);
+            if (rating != null)
+            {
+                _context.Rating.Remove(rating);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RatingExists(string id)
+        {
+          return (_context.Rating?.Any(e => e.Name == id)).GetValueOrDefault();
+        }
     }
 }
+
